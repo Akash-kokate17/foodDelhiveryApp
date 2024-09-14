01e8caa71f716c3a443131ab11453592becd7dc0
Akash-kokate17 <Akashkokate1717@gmail.com>
2024-09-14 14:00:26 +0530

    guest login button added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,20 +1,17 @@
 import React, { useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Swal from 'sweetalert2'
+import { Link, useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2';
 
 export default function Login(props) {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
  
-
   let navigate = useNavigate();
   let navigateSignUp = useNavigate();
   let emailRef = useRef();
   let passRef = useRef();
 
-
   const checkLogin = () => {
-    
     if (emailRef.current.value === '' || passRef.current.value === '') {
       Swal.fire({
         icon: "error",
@@ -23,12 +20,10 @@ export default function Login(props) {
       });
       return; 
     }
-    let getDataInfo = JSON.parse(window.localStorage.getItem('user'))
+    let getDataInfo = JSON.parse(window.localStorage.getItem('user'));
     if (getDataInfo.email === email && getDataInfo.Password === password) {
-     navigate('/restaurant')
-
-      }
-     else {
+      navigate('/restaurant');
+    } else {
       Swal.fire({
         icon: "error",
         title: "Oops...",
@@ -38,20 +33,35 @@ export default function Login(props) {
   }
 
   return (
-    <div className="container mt-5 container-fluid">
-      <div className="border border-2 m-auto p-4 rounded" style={{ minHeight: "300px", maxWidth: "400px" }}>
+    <div className="container d-flex justify-content-center align-items-center min-vh-100">
+      <div className="border border-2 p-4 rounded shadow-sm bg-white" style={{ maxWidth: '400px', width: '100%' }}>
         <div className="text-center mb-4">
           <h4>Login Page</h4>
         </div>
         <div className="mb-3">
-          <input type="email" className="form-control text-center" placeholder="Email" onChange={(e) => { setEmail(e.target.value) }} ref={emailRef}/>
+          <input 
+            type="email" 
+            className="form-control" 
+            placeholder="Email" 
+            onChange={(e) => { setEmail(e.target.value); }} 
+            ref={emailRef} 
+          />
         </div>
         <div className="mb-3">
-          <input className="form-control text-center" type="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value)  }} ref={passRef} />
+          <input 
+            className="form-control" 
+            type="password" 
+            placeholder="Password" 
+            onChange={(e) => { setPassword(e.target.value); }} 
+            ref={passRef} 
+          />
         </div>
-        <div className="mt-3 text-center">
-          <button className="btn btn-primary " onClick={checkLogin}>Login</button>
-          <button className="btn btn-danger  ms-5" onClick={() => { navigateSignUp('/signup') }}>Sign Up</button>
+        <div className="d-flex justify-content-evenly mt-3">
+          <button className="btn btn-primary" onClick={checkLogin}>Login</button>
+            <button className="btn btn-danger" onClick={() => { navigateSignUp('/signup'); }}>Sign Up</button>
+            <Link to="/restaurant">
+              <button className="btn btn-success">Guest Login</button>
+            </Link>
         </div>
       </div>
     </div>
